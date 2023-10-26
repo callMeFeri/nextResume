52b64f44cada24d194d9f57e2b3521bf6f61a19e
Dr.Feri <mmahdifarvardin@gmail.com>
2023-10-26 23:11:23 +0330

    few style changes

--- src/app/components/swrFetching/getData.tsx
+++ src/app/components/swrFetching/getData.tsx
@@ -18,7 +18,28 @@ function GetData({ url }: { url: string }) {
   });
 
   if (error) return <div className="pt-20">failed to load</div>;
-  if (isLoading) return <div className="pt-20">loading...</div>;
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center w-full h-full pt-20 pb-50">
+        <div className="flex justify-center items-center space-x-1 text-sm text-gray-700">
+          <svg
+            fill="none"
+            className="w-6 h-6 animate-spin"
+            viewBox="0 0 32 32"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              clip-rule="evenodd"
+              d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
+              fill="currentColor"
+              fill-rule="evenodd"
+            />
+          </svg>
+
+          <div>Loading ...</div>
+        </div>
+      </div>
+    );
   return (
     <div className="text-center pt-20 pb-10 ">
       <h1 className="text-7xl bg-gradient-to-b from-green-800 to-blue-300 bg-clip-text text-transparent">
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Dashboard = (): JSX.Element => {
-  return <div>Dashboard </div>;
+  return <div className="pt-20">Dashboard </div>;
 };
 
 export default Dashboard;
