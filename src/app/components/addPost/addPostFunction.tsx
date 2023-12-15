8e63bf7702102d84212059fb0ad88d001be929ac
Dr.Feri <mmahdifarvardin@gmail.com>
2023-12-15 20:51:46 +0330

    title adding to db fixed

--- src/app/components/addPost/addPostFunction.tsx
+++ src/app/components/addPost/addPostFunction.tsx
@@ -1,18 +1,19 @@
 import React, { FormEvent } from "react";
 import AddPostToast from "./addPostToast";
 
+import type { User } from "@/app/types/types";
+
 const AddPostFunction = async (e: FormEvent<HTMLFormElement>) => {
   //grabbing infos
   const url = `http://localhost:1337/api/posts`;
-  //merging new post and old one
-  const prevData = await fetch(url);
 
   //getiing the user name
-  const userName: string | null = localStorage.getItem("currentUserInfo");
+  const user: string = localStorage.getItem("User profile") as string;
+  const userName: User = JSON.parse(user).user.username;
 
   const postArr = JSON.stringify({
     data: {
-      title: (e.target as HTMLFormElement).title.valueOf,
+      title: (e.target as HTMLFormElement).title.value,
       postsContent: (e.target as HTMLFormElement).textmemory.value,
       userId: userName,
       postId: new Date().getTime().toString(),
